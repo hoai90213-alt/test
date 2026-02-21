6645af819d0f053485f8dfcec9576ef1f5f510d0
hoai90213-alt <hoai90213-alt@users.noreply.github.com>
2026-02-21 13:34:38 +0700

    Add Apple pthread initializer shim and BSD type defines

---
+++ app/src/main/cpp/box64/src/include/pthread.h
@@ -0,0 +1,12 @@
+#ifndef BOX64_COMPAT_PTHREAD_H
+#define BOX64_COMPAT_PTHREAD_H
+
+#include_next <pthread.h>
+
+#if defined(__APPLE__)
+#ifndef PTHREAD_ERRORCHECK_MUTEX_INITIALIZER_NP
+#define PTHREAD_ERRORCHECK_MUTEX_INITIALIZER_NP PTHREAD_MUTEX_INITIALIZER
+#endif
+#endif
+
+#endif
